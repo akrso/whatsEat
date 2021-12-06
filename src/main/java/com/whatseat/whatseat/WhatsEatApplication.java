5e43526b602f1a4e1246989441c071ed1aa5f3a1
akrso999 <akrso999@gmail.com>
2021-12-06 17:41:28 +0900

    create project

---
+++ src/main/java/com/whatseat/whatseat/WhatsEatApplication.java
@@ -0,0 +1,13 @@
+package com.whatseat.whatseat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhatsEatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WhatsEatApplication.class, args);
+    }
+
+}
